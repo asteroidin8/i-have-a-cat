588e0656129177573a5297ea4c0cccf9451c7e41
asteroidin8 <asteroidin8@gmail.com>
2026-05-27 19:16:43 +0900

    2차 MVP 데스크탑 동반자 경험 개선
    2차 MVP 데스크탑 동반자 경험 개선

--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,17 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
+const START_AT_LOGIN_CHANNEL = "app:set-start-at-login";
 const MOUSE_EVENTS_CHANNEL = "window:set-ignore-mouse-events";
+const WINDOW_SIZE_CHANNEL = "window:set-size";
 
 contextBridge.exposeInMainWorld("catWindow", {
+  setStartAtLogin(shouldOpenAtLogin) {
+    ipcRenderer.send(START_AT_LOGIN_CHANNEL, shouldOpenAtLogin);
+  },
   setIgnoreMouseEvents(shouldIgnore) {
     ipcRenderer.send(MOUSE_EVENTS_CHANNEL, shouldIgnore);
+  },
+  setWindowSize(size) {
+    ipcRenderer.send(WINDOW_SIZE_CHANNEL, size);
   },
 });
